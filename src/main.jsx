cc3008a763ce840a1859492bf4735163f1f2ea20
ThiagoSegalla47 <thiagosegalla147@gmail.com>
2025-12-29 22:23:55 -0300

    alguns ajustes de organização

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './aula19/App.jsx'
+import App from './fortunessegalla/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
